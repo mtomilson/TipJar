57c107952e12063bddcc9e774afa5604b7b7dd7c
Mathew Tomilson <mtomilson51@gmail.com>
2024-10-09 17:42:44 -0400

    fixed env leak changed firebase projects

--- tip-calculator/firebase.js
+++ tip-calculator/firebase.js
@@ -1,17 +1,17 @@
-
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "@firebase/firestore"
+import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "@firebase/auth";
 import {
-        FIREBASE_API_KEY, 
-        FIREBASE_AUTH_DOMAIN, 
-        FIREBASE_PROJECT_ID, 
-        FIREBASE_STORAGE_BUCKET, 
-        FIREBASE_MESSAGING_SENDER_ID, 
-        FIREBASE_APP_ID } from '@env'
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from "@env";
 
-import {getAuth} from "@firebase/auth"
-
+// Firebase configuration
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
@@ -19,11 +19,10 @@ const firebaseConfig = {
   storageBucket: FIREBASE_STORAGE_BUCKET,
   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
   appId: FIREBASE_APP_ID,
-
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase (Check if an instance already exists)
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const firestore = getFirestore(app)
-export const auth = getAuth(app)
+export const firestore = getFirestore(app);
+export const auth = getAuth(app);
