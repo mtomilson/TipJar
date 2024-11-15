1c2b703b65814287262ceb9401a899e1585a21b3
Mathew Tomilson <mtomilson51@gmail.com>
2024-11-14 21:06:47 -0500

    didnt' need trim

--- tip-calculator/firebase.js
+++ tip-calculator/firebase.js
@@ -13,7 +13,7 @@ import {
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY.trim(),
+  apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
   projectId: FIREBASE_PROJECT_ID,
   storageBucket: FIREBASE_STORAGE_BUCKET,
